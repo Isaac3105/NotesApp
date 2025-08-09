4c5e770ae3422d878f23978faccb8c37b10351f9
Isaac3105 <isaac.absantos@gmail.com>
2025-08-09 09:52:00 +0100

    Firebase setup and keys hidden

---
+++ tool/generate_firebase_files.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+
+  // ANDROID google-services.json
+  final androidJson = '''
+{
+  "project_info": {
+    "project_id": "${dotenv.env['FIREBASE_PROJECT_ID']}",
+    "storage_bucket": "${dotenv.env['FIREBASE_STORAGE_BUCKET']}"
+  },
+  "client": [
+    {
+      "client_info": {
+        "mobilesdk_app_id": "${dotenv.env['FIREBASE_APP_ID_ANDROID']}",
+        "android_client_info": {
+          "package_name": "${dotenv.env['FIREBASE_PACKAGE_NAME']}"
+        }
+      },
+      "api_key": [
+        {
+          "current_key": "${dotenv.env['FIREBASE_API_KEY_ANDROID']}"
+        }
+      ]
+    }
+  ],
+  "configuration_version": "1"
+}
+''';
+  File('android/app/google-services.json').writeAsStringSync(androidJson);
+  print('Generated android/app/google-services.json');
+
+  // iOS GoogleService-Info.plist
+  final iosPlist = '''
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+"http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>API_KEY</key>
+	<string>${dotenv.env['FIREBASE_API_KEY_IOS']}</string>
+	<key>APP_ID</key>
+	<string>${dotenv.env['FIREBASE_APP_ID_IOS']}</string>
+	<key>PROJECT_ID</key>
+	<string>${dotenv.env['FIREBASE_PROJECT_ID']}</string>
+	<key>STORAGE_BUCKET</key>
+	<string>${dotenv.env['FIREBASE_STORAGE_BUCKET']}</string>
+	<key>BUNDLE_ID</key>
+	<string>${dotenv.env['FIREBASE_IOS_BUNDLE_ID']}</string>
+	<key>GCM_SENDER_ID</key>
+	<string>${dotenv.env['FIREBASE_MESSAGING_SENDER_ID']}</string>
+</dict>
+</plist>
+''';
+  File('ios/Runner/GoogleService-Info.plist').writeAsStringSync(iosPlist);
+  print('Generated ios/Runner/GoogleService-Info.plist');
+}
