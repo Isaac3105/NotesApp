39afff4d7231e03d6eb3a0399494c9d7cc2e87d8
Isaac3105 <isaac.absantos@gmail.com>
2025-08-24 21:29:01 +0100

    Tested a bit with blocs. Created a simple counter. Next commit will be about converting the existing auth process to bloc.

--- lib/views/route_view.dart
+++ lib/views/route_view.dart
@@ -1,33 +1,180 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:to_do_app/services/auth/auth_service.dart';
 import 'package:to_do_app/views/login_view.dart';
 import 'package:to_do_app/views/notes/notes_view.dart';
 import 'verify_email_view.dart';
 
-class RouteView extends StatelessWidget {
+// class RouteView extends StatelessWidget {
+//   const RouteView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//       future: AuthService.firebase().initialize(),
+//       builder: (context, asyncSnapshot) {
+//         switch (asyncSnapshot.connectionState) {
+//           case ConnectionState.done:
+//             final user = AuthService.firebase().currentUser;
+//             if (user != null) {
+//               if (user.isEmailVerified) {
+//                 return const ToDoView();
+//               } else {
+//                 return const VerifyEmailView();
+//               }
+//             } else {
+//               return const LoginView();
+//             }
+//           default:
+//             return const Center(child: CircularProgressIndicator());
+//         }
+//       },
+//     );
+//   }
+// }
+
+class RouteView extends StatefulWidget {
   const RouteView({super.key});
 
   @override
+  State<RouteView> createState() => _RouteViewState();
+}
+
+class _RouteViewState extends State<RouteView> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: AuthService.firebase().initialize(),
-      builder: (context, asyncSnapshot) {
-        switch (asyncSnapshot.connectionState) {
-          case ConnectionState.done:
-            final user = AuthService.firebase().currentUser;
-            if (user != null) {
-              if (user.isEmailVerified) {
-                return const ToDoView();
-              } else {
-                return const VerifyEmailView();
-              }
-            } else {
-              return const LoginView();
-            }
-          default:
-            return const Center(child: CircularProgressIndicator());
-        }
-      },
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Counter"),
+          backgroundColor: Colors.amber,
+        ),
+        body: BlocConsumer<CounterBloc, CounterState>(
+          builder: (context, state) {
+            final invalidValue = (state is CounterStateInvalidNumber)
+                ? state.invalidValue
+                : "";
+            return Column(
+              children: [
+                Text("Current value is ${state.value}"),
+                Visibility(
+                  visible: (state is CounterStateInvalidNumber),
+                  child: Text("Invalid input $invalidValue"),
+                ),
+                TextField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    hintText: "Enter a number here",
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        context.read<CounterBloc>().add(
+                          IncrementEvent(_controller.text),
+                        );
+                      },
+                      child: const Icon(Icons.add),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        context.read<CounterBloc>().add(
+                          DecrementEvent(_controller.text),
+                        );
+                      },
+                      child: const Icon(Icons.remove),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+          listener: (context, state) {
+            _controller.clear();
+          },
+        ),
+      ),
     );
+  }
+}
+
+@immutable
+abstract class CounterState {
+  final int value;
+  const CounterState(this.value);
+}
+
+class CounterStateValid extends CounterState {
+  const CounterStateValid(super.value);
+}
+
+class CounterStateInvalidNumber extends CounterState {
+  final String invalidValue;
+  const CounterStateInvalidNumber({
+    required this.invalidValue,
+    required int previousValue,
+  }) : super(previousValue);
+}
+
+@immutable
+abstract class CounterEvent {
+  final String value;
+  const CounterEvent(this.value);
+}
+
+class IncrementEvent extends CounterEvent {
+  const IncrementEvent(super.value);
+}
+
+class DecrementEvent extends CounterEvent {
+  const DecrementEvent(super.value);
+}
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(const CounterStateValid(0)) {
+    on<IncrementEvent>((event, emit) {
+      final integer = int.tryParse(event.value);
+      if (integer != null) {
+        emit(CounterStateValid(state.value + integer));
+      } else {
+        emit(
+          CounterStateInvalidNumber(
+            invalidValue: event.value,
+            previousValue: state.value,
+          ),
+        );
+      }
+    });
+    on<DecrementEvent>((event, emit) {
+      final integer = int.tryParse(event.value);
+      if (integer != null) {
+        emit(CounterStateValid(state.value - integer));
+      } else {
+        emit(
+          CounterStateInvalidNumber(
+            invalidValue: event.value,
+            previousValue: state.value,
+          ),
+        );
+      }
+    });
   }
 }
