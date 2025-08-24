53fb2828b20fea1c92da6ae0d227ca80e50f6db9
Isaac3105 <isaac.absantos@gmail.com>
2025-08-24 22:04:46 +0100

    Created some auth states

---
+++ lib/services/auth/bloc/auth_state.dart
@@ -0,0 +1,35 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_app/services/auth/auth_user.dart';
+
+@immutable
+abstract class AuthState {
+  const AuthState();
+}
+
+class AuthStateLoading extends AuthState {
+  const AuthStateLoading();
+}
+
+class AuthStateLoggedIn extends AuthState {
+  final AuthUser user;
+  const AuthStateLoggedIn({required this.user});
+}
+
+class AuthStateLogInFailure extends AuthState {
+  final Exception exception;
+  const AuthStateLogInFailure({required this.exception});
+}
+
+class AuthStateNeedsVerification extends AuthState {
+  const AuthStateNeedsVerification();
+}
+
+class AuthStateLoggedOut extends AuthState {
+  const AuthStateLoggedOut();
+}
+
+class AuthStateLogOutFailure extends AuthState {
+  final Exception exception;
+  const AuthStateLogOutFailure({required this.exception});
+}
